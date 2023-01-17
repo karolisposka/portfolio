1f9bb3b870646c8de24e5ff3e1230a5a20ef9369
karolisposka <karolisposka96@gmail.com>
2023-01-17 18:05:47 +0200

    projectCard component render effected disabled

--- src/components/project/ProjectsCard.tsx
+++ src/components/project/ProjectsCard.tsx
@@ -6,43 +6,41 @@ type props = {
   title: string;
   text: string;
   to: string | number;
-  scrolled: boolean;
-  index: number;
+  scrolled?: boolean;
+  index?: number;
 };
 
 const ProjectsCard = ({ image, title, text, to, scrolled, index }: props) => {
-  const [display, setDisplay] = useState(false);
+  // const [display, setDisplay] = useState(false);
 
   //this effect is used to identify when intersepction observer slides to right place and set delay to rendering
 
-  useEffect(() => {
-    const delay = index * 700;
-    if (scrolled) {
-      setTimeout(() => {
-        setDisplay(true);
-        setTimeout(() => {
-          if (index !== 1) {
-            const position = window.scrollY;
-            window.scrollTo({ top: position + 500, behavior: "smooth" });
-          }
-        }, 200);
-      }, delay);
-    }
-    return;
-  }, [scrolled, index]);
+  // useEffect(() => {
+  //   const delay = index * 700;
+  //   if (scrolled) {
+  //     setTimeout(() => {
+  //       setDisplay(true);
+  //       setTimeout(() => {
+  //         if (index !== 1) {
+  //           const position = window.scrollY;
+  //           window.scrollTo({ top: position + 500, behavior: "smooth" });
+  //         }
+  //       }, 200);
+  //     }, delay);
+  //   }
+  //   return;
+  // }, [scrolled, index]);
 
   return (
     <>
-      {display && (
-        <S.Container>
-          <S.Image src={image} alt="text" />
-          <S.InfoWrapper>
-            <S.Title>{title}</S.Title>
-            <S.Text>{text}</S.Text>
-            <S.LinkBtn type="link" to={to} text="Learn more" />
-          </S.InfoWrapper>
-        </S.Container>
-      )}
+      <S.Container>
+        <S.Image src={image} alt="text" />
+        <S.InfoWrapper>
+          <S.Title>{title}</S.Title>
+          <S.Text>{text}</S.Text>
+          <S.LinkBtn type="link" to={to} text="Learn more" />
+        </S.InfoWrapper>
+      </S.Container>
     </>
   );
 };
