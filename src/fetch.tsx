01589d73a0ab27f852bf39c8b2aeb0b9cc5cf975
karolisposka <karolisposka96@gmail.com>
2023-01-17 17:20:43 +0200

    fixes

--- src/fetch.tsx
+++ src/fetch.tsx
@@ -5,21 +5,21 @@ import { Outlet } from "react-router-dom";
 import Loader from "./components/loader/Loader";
 
 const Fetch = () => {
-  const [data, setBio, setData] = useContext(MainPageData);
+  const [data, bio, setBio, setData] = useContext(MainPageData);
   const base = process.env.REACT_APP_BASE_URL;
+
+  console.log(data);
 
   const fetchData = async () => {
     try {
       const res = await fetch(`${base}/api/bios/?populate=teches`);
       const response = await fetch(`${base}/api/projects/?populate=thumbnail`);
       const bio = await res.json();
-
       setBio(bio.data);
-
       const data = await response.json();
-
       setData(data.data);
     } catch (err) {
+      console.log(err);
       console.log("Oops! Something wrong with server. Please try again later");
     }
   };
@@ -28,7 +28,7 @@ const Fetch = () => {
     fetchData();
   }, []);
 
-  return data ? <Outlet /> : <Loader />;
+  return data && bio ? <Outlet /> : <Loader />;
 };
 
 export default Fetch;
